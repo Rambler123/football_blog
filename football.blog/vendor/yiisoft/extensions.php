b6b5ec38a020f089ceb1039a0c4bc6e91ca33ead
Rambler123 <optimist_it@mail.ru>
2017-08-25 00:20:11 +0300

    Next
    Next

---
+++ football.blog/vendor/yiisoft/extensions.php
@@ -0,0 +1,132 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'kartik-v/yii2-widget-fileinput' => 
+  array (
+    'name' => 'kartik-v/yii2-widget-fileinput',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik/file' => $vendorDir . '/kartik-v/yii2-widget-fileinput',
+    ),
+  ),
+  'newerton/yii2-fancybox' => 
+  array (
+    'name' => 'newerton/yii2-fancybox',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@newerton/fancybox' => $vendorDir . '/newerton/yii2-fancybox',
+    ),
+  ),
+  'yiisoft/yii2-imagine' => 
+  array (
+    'name' => 'yiisoft/yii2-imagine',
+    'version' => '2.1.0.0',
+    'alias' => 
+    array (
+      '@yii/imagine' => $vendorDir . '/yiisoft/yii2-imagine',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'kartik-v/yii2-krajee-base' => 
+  array (
+    'name' => 'kartik-v/yii2-krajee-base',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik/base' => $vendorDir . '/kartik-v/yii2-krajee-base',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'linchpinstudios/yii2-backstretch' => 
+  array (
+    'name' => 'linchpinstudios/yii2-backstretch',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@linchpinstudios/backstretch' => $vendorDir . '/linchpinstudios/yii2-backstretch',
+    ),
+  ),
+  'bluezed/yii2-scroll-top' => 
+  array (
+    'name' => 'bluezed/yii2-scroll-top',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@bluezed/scrollTop' => $vendorDir . '/bluezed/yii2-scroll-top',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'pudinglabs/yii2-bootstrap-tags-input' => 
+  array (
+    'name' => 'pudinglabs/yii2-bootstrap-tags-input',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@pudinglabs/tagsinput' => $vendorDir . '/pudinglabs/yii2-bootstrap-tags-input',
+    ),
+  ),
+  'faryshta/yii2-jquery-tagsinput' => 
+  array (
+    'name' => 'faryshta/yii2-jquery-tagsinput',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@faryshta' => $vendorDir . '/faryshta/yii2-jquery-tagsinput',
+    ),
+  ),
+  'kartik-v/yii2-widget-select2' => 
+  array (
+    'name' => 'kartik-v/yii2-widget-select2',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik/select2' => $vendorDir . '/kartik-v/yii2-widget-select2',
+    ),
+  ),
+);
